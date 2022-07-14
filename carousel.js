2a8b2f1ee3b42f00799738fafe3600d2576af2aa
josefienMatla <55492557+josefienMatla@users.noreply.github.com>
2022-07-14 16:34:16 +0200

    My first commit! 😄

---
+++ carousel.js
@@ -0,0 +1,77 @@
+const swiper = new Swiper(".swiper--tippie", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 90,
+    grabCursor: true,
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const swiper2 = new Swiper(".swiper--tripple", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 90,
+    grabCursor: true,
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const swiper3 = new Swiper(".swiper--vr", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 90,
+    grabCursor: true,
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 60,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
